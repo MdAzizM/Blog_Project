a8fd78442d4e83cc9de3387209b8cce5c44e9250
MdAzizM <azizmazghouni456@gmail.com>
2024-11-20 15:21:23 +0100

    Functionnal CRUD User features

--- Backend/Controllers/UserControllers.js
+++ Backend/Controllers/UserControllers.js
@@ -1,44 +1,95 @@
 const mongoose = require ('mongoose');
+const bcrypt = require('bcrypt');
 const User = require ('../Models/User');
 
-const Get_User = (req, res) => {
-    const { id } = req.params;
+const Get_All_Users =async (req, res) => {
     try{
-        console.log('get user is working');
+        const users = await User.find().sort({ createdAt: -1 });
+        if (users){
+            res.status(200).json(users);
+        }
     }catch(err){
         res.status(404).json({"msg":err})
     }
 }
 
-const Create_User = (req, res) => {
+const Get_User =async (req, res) => {
+    const { id } = req.params;
     try{
-        console.log('create user is working');
+        const user = await User.findById(id);
+        if(user){
+            res.status(200).json(user);
+        }
     }catch(err){
-        res.status(404).json({"msg":err})
+        res.status(404).json({"Read Error msg":err})
     }
 }
 
-const Update_User = (req, res) => {
-    const { id } = req.params;
+const Create_User =async (req, res) => {
+    const { First_name, Last_name, Email, Password } = req.body;
+    //const crypted_password = await bcrypt.hash({ Password }, 10, process.env.HASH_KEY);
+    const user = new User({ First_name, Last_name, Email, Password });
     try{
-        console.log('update user is working');
+        console.log(user);  
+        const newUser = await user.save();
+        if (newUser){
+            res.status(200).json({ message:"user created succefuly",newUser});
+        }
     }catch(err){
-        res.status(404).json({"msg":err})
+        res.status(400).json({"Create User Error msg":err})
     }
 }
 
-const Delete_User = (req, res) => {
+const Update_User =async (req, res) => {
     const { id } = req.params;
+    const { First_name, Last_name, Email, Password } = req.body;
     try{
-        console.log('delete user is working');
+        const Updated_User = await User.findByIdAndUpdate(id, { First_name, Last_name, Email, Password })
+        if (Updated_User){
+            res.status(200).json({Message:"User updated!", Updated_User});
+        }
     }catch(err){
-        res.status(404).json({"msg":err})
+        res.status(404).json({"Update (all) msg":err});
+    }
+}
+
+const Update_Onething_User = async(req, res) => {
+    const { id } = req.params;
+    const updateData = req.body; 
+    try {
+        const user = await User.findByIdAndUpdate(id, updateData, { new: true });
+        if (!user) {
+            return res.status(404).json({ message: 'User not found'});
+        }
+        res.status(200).json(user);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+}
+
+const Delete_User =async (req, res) => {
+    const { id } = req.params;
+    try{    
+        
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).json({error: 'No such USER'})
+        }
+
+        const deletedUser = await User.findByIdAndDelete(id);
+        if (deletedUser){
+            res.status(200).json({"msg":"user deleted"})
+        }
+
+    }catch(err){
+        res.status(404).json({"Delete User error ":err.message});
     }
 }
 
 module.exports = {
+    Get_All_Users,
     Get_User,
     Create_User,
     Update_User,
+    Update_Onething_User,
     Delete_User
 }
--- Backend/Routes/UserRoutes.js
+++ Backend/Routes/UserRoutes.js
@@ -3,11 +3,15 @@ const UserControllers = require ('../Controllers/UserControllers');
 
 const route = Router();
 
+route.get('/', UserControllers.Get_All_Users);
+
 route.get ('/:id', UserControllers.Get_User);
 
 route.post('/', UserControllers.Create_User);
 
 route.put ('/:id', UserControllers.Update_User);
+
+route.patch('/:id', UserControllers.Update_Onething_User);
 
 route.delete('/:id', UserControllers.Delete_User);
 
